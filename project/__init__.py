968397928ec97ec4a7aa76b0cb47d4b458bff921
subut0n <a.dzikowski@outlook.fr>
2022-07-01 14:11:02 +0200

    sentry starts

--- project/__init__.py
+++ project/__init__.py
@@ -2,8 +2,14 @@ from venv import create
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+
 from dotenv import load_dotenv
 import os
+
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
@@ -12,6 +18,18 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+
+    sentry_sdk.init(
+        dsn="https://62f8d8dea17c4a36a179f3abffed88d9@o1303935.ingest.sentry.io/6543498",
+        integrations=[
+            FlaskIntegration(),
+        ],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0
+    )
 
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
